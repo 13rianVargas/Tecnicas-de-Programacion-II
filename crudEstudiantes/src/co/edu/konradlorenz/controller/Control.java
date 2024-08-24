5441d01cc9a4be35a04ce55d43919a84e6da9bad
13rianSVargasC <brians.vargasc@konradlorenz.edu.co>
2024-08-24 00:04:20 -0500

    Creo ArrayList

--- crudEstudiantes/src/co/edu/konradlorenz/controller/Control.java
+++ crudEstudiantes/src/co/edu/konradlorenz/controller/Control.java
@@ -1,16 +1,26 @@
 package co.edu.konradlorenz.controller;
 
+import java.util.ArrayList;
+
 import co.edu.konradlorenz.model.Estudiante;
+//import co.edu.konradlorenz.view.Formulario; //No longer useful//
 
 public class Control {
 	
 	// - // - // Objects // - // - //
 	protected Estudiante estudiante = new Estudiante();
-	
+	//protected Formulario formulario = new Formulario(); //No longer useful//
+	private ArrayList<Estudiante> listaDeEstudiantes = new ArrayList<>();
 	
 	// - // - // Methods // - // - //
 	public void run() {
 		//TODO: Crear la lista de pasos que realiza el Control.
+		//1. Get data
+		//estudiante.
+		
+		
+		//2.
+		//3.
 	}
 	
 }
