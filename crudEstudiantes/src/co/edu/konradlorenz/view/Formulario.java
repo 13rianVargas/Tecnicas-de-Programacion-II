9a0e5931be4a951819131d0348059ec6962d8d29
13rianSVargasC <brians.vargasc@konradlorenz.edu.co>
2024-08-24 00:05:22 -0500

    Creo métodos "pedir" y demás métodos  obligatorios

--- crudEstudiantes/src/co/edu/konradlorenz/view/Formulario.java
+++ crudEstudiantes/src/co/edu/konradlorenz/view/Formulario.java
@@ -8,20 +8,50 @@ public class Formulario {
 	static Scanner leer = new Scanner(System.in);
 	
 	
-	// - // - // Methods // - // - //TODO: crear todos los "pedir" de los atributos que necesito.
-	public static long pedirLong(){//Mandatory
-		return 0;
+	// - // - // Methods // - // - //
+	public static String pedirNombres(String nombres){//Mandatory pedirString(String):String;
+		System.out.println("Ingrese los nombres del estudiante: ");
+		nombres = leer.nextLine();
+		return nombres;
 	}
 	
-	public static String pedirString(String xd){//Mandatory
-		return "";
-	}
-
-	public static void mostrarEstudiante(String palabraDeBusqueda){//Mandatory
-		
+	public static String pedirApellidos(/*/String apellidos/*/){
+		System.out.println("Ingrese los apellidos del estudiante: ");
+		return leer.nextLine();
 	}
 	
-	public static int pedirEntero(String xd) {//Mandatory
-		return 0;
+	public static long pedirCodigo(){//Mandatory pedirLong():long;
+		System.out.println("Ingrese el código del estudiante: ");
+		return leer.nextLong();
+	}
+	
+	public static byte pedirEdad(){
+		System.out.println("Ingrese la edad del estudiante: ");
+		return leer.nextByte();
+	}
+	
+	public static String pedirPrograma(/*/String programa/*/){
+		System.out.println("Ingrese el programa del estudiante: ");
+		return leer.nextLine();
+	}
+	
+	public static String pedirCorreo(/*/String correo/*/){
+		System.out.println("Ingrese el correo del estudiante: ");
+		return leer.nextLine();
+	}
+	
+	public static byte pedirNumeroMatriculo(){
+		System.out.println("Ingrese el número de matrícula del estudiante: ");
+		return leer.nextByte();
+	}
+	
+	public static void mostrarEstudiante(String estudianteInfo){//Mandatory mostrarEstudiante(String):void;
+		System.out.println(estudianteInfo);
+	}
+	
+	public static int pedirEntero(String numero) {//Mandatory pedirEntero(String):int;
+		System.out.println("Selecciona el número del estudiante: ");
+		//TODO:Método para mostrar la lista de estudiantes.
+		return leer.nextInt();
 	}
 }
