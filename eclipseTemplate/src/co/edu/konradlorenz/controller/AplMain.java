98efa0a2a771cfdf055a48b8fdd607eba6671b8b
13rian Vargas <bsvargasc13@gmail.com>
2024-09-19 19:05:04 -0500

    eclipseTemplate Update
    Añado clases Controlador y Vista genéricas, con sus los objetos para el controlador en el AplMain.

--- eclipseTemplate/src/co/edu/konradlorenz/controller/AplMain.java
+++ eclipseTemplate/src/co/edu/konradlorenz/controller/AplMain.java
@@ -3,7 +3,7 @@ package co.edu.konradlorenz.controller;
 public class AplMain {
 
 	public static void main(String[] args) {
-
+		Controlador controlador = new Controlador();
+		controlador.run();
 	}
-
 }
---
+++ eclipseTemplate/src/co/edu/konradlorenz/view/Vista.java
@@ -0,0 +1,11 @@
+package co.edu.konradlorenz.view;
+
+import java.util.Scanner;
+
+public class Vista {
+    Scanner sc = new Scanner(System.in);
+    
+    public void mostrarMensaje(String mensaje){
+        System.out.println("");
+    }
+}
